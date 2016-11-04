35e8603883654f45cb1dc4b956e09cc01d1bf29b
John Lee <lee1048@llnl.gov>
2016-11-04 16:26:44 -0700

    Initial creation of api view completed

---
+++ WindowsApp/Models/ApiModel.cs
@@ -0,0 +1,24 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Models
+{
+    public class ApiModel
+    {
+        private string _apiUrl;
+
+        public ApiModel(string apiUrl)
+        {
+            _apiUrl = apiUrl;
+            DataType = "JSON";
+            DataContent = JSONReader.GetJSONFromUrl(apiUrl);
+        }
+
+        public string DataContent { get; set; }
+        public string DataType { get; set; }
+    }
+}
--- WindowsApp/ViewModels/APIViewModel.cs
+++ WindowsApp/ViewModels/APIViewModel.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WindowsApp.Models;
 
 namespace WindowsApp
 {
-    class ApiViewModel
+    public class ApiViewModel : ObservableObject
     {
+        private string _apiUrl = "https://jsonplaceholder.typicode.com/posts";
+        private readonly ObservableCollection<string> _results = new ObservableCollection<string>();
+
+        public string Url
+        {
+            get { return _apiUrl; }
+            set
+            {
+                _apiUrl = value;
+                RaisePropertyChangedEvent("ApiUrl");
+            }
+        }
+
+        public ICommand Load
+        {
+            get { return new Command(LoadDataFromApiUrl); }
+        }
+
+        public IEnumerable<string> Results
+        {
+            get { return _results; }
+        }
+
+        // Delegate method definitions
+        #region Delegates
+
+        private void LoadDataFromApiUrl()
+        {
+            if (string.IsNullOrEmpty(Url))
+            {
+                return;
+            }
+            ApiModel model = new ApiModel(Url);
+            _results.Add("Data Type: " + model.DataType);
+            _results.Add("Contents: " + model.DataContent);
+        }
+
+        #endregion
     }
 }
